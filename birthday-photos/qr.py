de0eed763394cffb61d0a2d412a6cb1b63fe0a17
Nikitha Kantha <nikithakantha@gmail.com>
2026-04-03 13:49:18 +0530

    Fix it

--- birthday-photos/qr.py
+++
@@ -1,6 +0,0 @@
-import qrcode
-qr = qrcode.make("http://127.0.0.1:5500/birthday-photos/index.html")
-qr.save("qr.png")
-print("QR code generated and saved as qr.png")
-print("You can open qr.png to view the QR code, which will direct you to the birthday photos webpage.")
-print(qr)
